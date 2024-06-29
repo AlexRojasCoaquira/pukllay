eec9d59500fdb9c63819face07260bc75f7828fa
AlexRojasCoaquira <rommel.alex.rc@gmail.com>
2024-06-28 19:52:02 -0500

    muestra

--- src/components/ui/AuthLove.vue
+++ src/components/ui/AuthLove.vue
@@ -2,7 +2,7 @@
   <div class="w-96 p-10 bg-[#000000c8] rounded">
     <div class="content-auth">
       <div class="">
-        <h1 class="text-center text-white text-2xl font-bold mb-5">EL DÍA QUE NOS CONOCIMOS</h1>
+        <h1 class="text-center text-white text-2xl font-bold mb-5">NUESTRA PRIMERA EMPRESA</h1>
         <InputCustom v-model="key"></InputCustom>
         <ButtonCustom class="text-center" label="Verificar" @click="verifyKey()"></ButtonCustom>
       </div>
