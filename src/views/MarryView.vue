5571653554f3cff22d9e99ceb00300cd303f6e9e
AlexRojasCoaquira <rommel.alex.rc@gmail.com>
2024-06-28 20:57:35 -0500

    feat: upload image marry

--- src/views/MarryView.vue
+++ src/views/MarryView.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="about bg-red-500 mt-15">
-    <h1 class="text-5xl">This is an about page</h1>
+  <div class="page">
+    <div class="flex justify-center my-2">
+      <img src="../assets/images/matrimonio.jpeg" alt="" />
+    </div>
   </div>
 </template>
 
